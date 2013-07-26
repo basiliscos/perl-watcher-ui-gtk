b57b932ba2854f1249e98dac93b3f63d9e1ab4ca
Ivan Baidakou <dmol@gmx.com>
2013-07-27 00:53:53 +0300

    Build results of c945f36 (on master)

--- lib/App/PerlWatcher/UI/Gtk2/Utils.pm
+++ lib/App/PerlWatcher/UI/Gtk2/Utils.pm
@@ -19,12 +19,11 @@ use parent qw/Exporter/;
 
 our @EXPORT_OK = qw/get_level_icon/;
 
-#memoize('get_level_icon');
+memoize('get_level_icon');
 sub get_level_icon {
     my ($level, $unseen) = @_;
     my $postfix = $unseen ? "_new" : "";
     my $filename = dist_file('App-PerlWatcher-UI-Gtk2', "assets/icons/${level}${postfix}.png");
-    ### $filename
     return unless -r $filename;
     my @icon_size = Gtk2::IconSize->lookup('menu');
     my $pixbuff = Gtk2::Gdk::Pixbuf->new_from_file_at_scale($filename, @icon_size, 1);
